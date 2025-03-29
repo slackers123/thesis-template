e0ccc0281fa256928491826040e9b32c73f242c3
slackers <srekcals@proton.me>
2025-03-29 10:49:51 +0100

    Update watch.ps1

--- watch.ps1
+++ watch.ps1
@@ -1,22 +1,46 @@
 $src_dir = "src"
-$output_dir = "out/"
+$output_dir = "out"
 
 # Run the build script
-./build.ps1
+#./build.ps1
 
 # Check if the --open flag is passed
 if ($args -contains "--open") {
     Start-Process "$output_dir/main.pdf"
 }
 
-# Watch for changes in the source directory
-Get-ChildItem -Recurse $src_dir | ForEach-Object {
-    Register-ObjectEvent -InputObject (Get-Item $_.FullName) -EventName Changed -Action {
-        ./build.ps1
+$File = "$src_dir\"
+$Action = './build.ps1'
+$global:FileChanged = $false
+
+function Wait-FileChange {
+    param(
+        [string]$File,
+        [string]$Action
+    )
+    $ScriptBlock = [scriptblock]::Create($Action)
+
+    $AbsFile = Resolve-Path $File
+
+    $Watcher = New-Object IO.FileSystemWatcher
+    $Watcher.Path = $AbsFile
+    $Watcher.IncludeSubdirectories = $true
+    $Watcher.EnableRaisingEvents = $true  
+
+    $onChange = Register-ObjectEvent $Watcher Changed -Action {$global:FileChanged = $true}
+
+    while ($true) {
+        while ($global:FileChanged -eq $false){
+           Start-Sleep -Milliseconds 100
+        }
+
+    	& $ScriptBlock 
+  	
+	$global:FileChanged = $false
     }
+
+
+    Unregister-Event -SubscriptionId $onChange.Id
 }
 
-# Keep the script running to monitor changes
-while ($true) {
-    Start-Sleep -Seconds 1
-}
+Wait-FileChange -File $File -Action $Action
